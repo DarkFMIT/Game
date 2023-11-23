f9d302d9406bb71fc4befe5e713cd014d53a6539
s0r1k <klimenko.m@phystech.edu>
2023-11-23 23:54:05 +0300

    road changes

--- MyGame/road.py
+++ MyGame/road.py
@@ -3,13 +3,19 @@ from screen import Sc, game
 
 class Road(Objects_for_build):
     def __init__(self, coords):
-        # ... (ваш код)
+        self.coords = [coords[0] - 60, coords[1] - 30]
+        self.type = pygame.image.load(".\MyGame\house.png")
+        self.game = game
+        self.prise = 100
+        self.key = "house" 
+        self.screen = Sc
+        self.dopusc = [0, 2]
 
     def buy_road(self):
         self.buy()
 
-    def buf_people(self):
+    def buf_people():
         pass
 
-    def buf_economic(self):
+    def buf_economic():
         pass
