35cbadc045c5a2e49daed37d532403fa8f69edde
DarkFMIT <danilius82@gmail.com>
2023-11-27 12:37:08 +0300

    Ксюша

--- for_ksusha/pyton.py
+++ for_ksusha/pyton.py
@@ -1,2 +1,31 @@
 import os
-os.system(".\\for_ksusha\\Hello.exe >.\\for_ksusha\\txt.txt")
+cor = 0
+incor = 0
+done = True
+file = open("./names_of_test", "r+")
+while done:
+    try:
+        str = file.readline()
+        os.system(f".\\main.exe {str} >.\\tmp.txt 2>&1")
+        correct_results = open(f"./correct/correct_for_{str}", "r+")
+        my_results = open("./tmp.txt")
+        done1 = True
+        while done1:
+            try:
+                correct = correct_results.readline()
+                tmp = my_results.readline()
+                if correct != tmp:
+                    incor += 1
+                    done1 = True
+                    correct_results.close()
+                    my_results.close
+            finally:
+                done1 = False
+                cor += 1
+                correct_results.close()
+                my_results.close
+
+    finally:
+        done = False
+        file.close()
+print(f"Correct: {cor}, Incorrect: {incor}")
