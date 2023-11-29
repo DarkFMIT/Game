24fa251c682002e4dace13db45f1da6acf67cd20
DarkFMIT <danilius82@gmail.com>
2023-11-29 14:28:48 +0300

    Упс.... Исправил

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from game import Game
-from screen import Screen
+from screen_opti import Screen
 from objects_for_build import Objects_for_build
 from house import House
+from road import Road
+from menu_test import  Menu
 import pygame
+pygame.font.init()
+
+
+
 
 game = Game()
 screen = Screen(game)
+menu = Menu(screen)
 done = False
 screen.update_window()
 while not done:
+    game.add_time()
+    menu.update_menu()
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             done = True
@@ -17,6 +26,7 @@ while not done:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 3:
                 pygame.mouse.get_rel()
+                screen.prev_x = -2
                 while pygame.mouse.get_pressed():
                     events = pygame.event.get()
                     if(len(events) != 0):
@@ -26,12 +36,26 @@ while not done:
                             moving = pygame.mouse.get_rel()
                             moving = screen.move(moving)
                             screen.update_window()
+                    game.add_time()
+                    menu.update_menu()
             if event.button == 1:
-                position = screen.mark_plate(event.pos)
+                if event.pos[1] > screen.size[1] - 30:
+                    if (screen.prev_x != -2):
+                        menu.choose(event.pos, position)
+                        print(menu.prev)
+                else:
+                    position = screen.mark_plate(event.pos)
+                    menu.update_menu()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_b and screen.prev_x != -2:
+            road = Road(screen, position)
+            road.buy()
+            screen.update_window()
+            menu.update_menu()
+            screen.prev_x = -2
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_h and screen.prev_x != -2:
             house = House(screen, position)
             house.buy()
             screen.update_window()
-            prev_x = -2
-            
+            menu.update_menu()
+            screen.prev_x = -2
     pygame.display.flip()
