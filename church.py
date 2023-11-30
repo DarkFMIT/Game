02045545fb7b6cace6a60d0bf85afd1541c2b1df
s0r1k <klimenko.m@phystech.edu>
2023-11-30 22:24:47 +0300

    A lot

---
+++ church.py
@@ -0,0 +1,47 @@
+from objects_for_build import Objects_for_build
+from game import Game
+import pygame
+
+"""
+    B классе хранятся:
+        Иконка                - .icon  [120 * 200]
+        Координаты отрисовки  - .points_for_build
+        Допустимые клетки     - .dopusc
+        Цена                  - .prise
+        Игра                  - .game
+        Экран                 - .screen    
+        Можно ли строить      - .can   
+"""
+class Church(Objects_for_build):
+
+    # Создание дома для конкретной клетки
+    # Требует окно и координаты
+    # Возврата нет
+    def __init__(self, screen, pos, icon_name):
+        icon = pygame.image.load(f".\\resources\\Buildings\{icon_name}.png")
+        self.dopusc = [0, 2]
+        self.prise = 10000
+        self.score = 500
+        self.capacity = 0
+        super().__init__(screen, pos, icon)
+        self.can = self.screen.can_build(self)
+
+    # Покупка здания
+    # Параметры не требует
+    # Возврата нет
+    def buy(self):
+        if(self.can == "True"):
+            self.screen.buy_building(self)
+            Game.church_number += 1 # (4) счетчик госпиталей
+            Game.score += self.score
+        else:
+            self.screen.show_error(self.can)
+
+    # Возвращает количество очков, которое приносит это здание  
+    def get_score(self):
+        return self.score
+
+    # Возвращает количество мест в этом здании
+    def get_capacity(self):
+        return self.capacity
+    
--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 from time import time as tm
 from random import randint
-# from hospital import Hospital
 """
     Глобально:
         Количество больниц              -   hospital_number
@@ -21,17 +20,26 @@ from random import randint
 
 """
 class Game:
-    hospital_number = 0 # (4) счетчик госпиталей
-    house_number = 0 # (5) счетчик домов
+    hospital_number = 0 
+    hospital_capacity = 200
+    house_number = 0
     school_number = 0
+    school_capacity = 200
     police_number = 0
+    police_capacity = 200
     president_number = 0
     university_number = 0
-    fire_number = 0
+    university_capacity = 200
+    firestation_number = 0
+    firestation_capacity = 200
     church_number = 0
+    church_capacity = 200
     cemetery_number = 0
+    cemetery_capacity = 200
     factory_number = 0
+    facatory_capacity = 200
     dump_number = 0
+    dump_capacity = 200
     score = 0
     # Задача начальных параметров, котрые нужны при старте игры.
     # Параметры не требует
@@ -99,9 +107,22 @@ class Game:
         self.citizens += death_rate // 10
 
     def adjusts_score(self):
-            if (self.citizens - Game.hospital_number * 200 > 0):
-                Game.score -= (self.citizens - Game.hospital_number * 200) // 100 # (1) Кайф формула
-            else:
+            hospital_avaiability = self.citizens - Game.hospital_number * Game.hospital_capacity
+            church_avaiability = self.citizens - Game.church_number * Game.church_capacity
+            police_avaiability = self.citizens - Game.police_number * Game.police_capacity
+            firestation_avaiability = self.citizens - Game.firestation_number * Game.firestation_capacity
+            cemetery_avaiability = self.citizens - Game.cemetery_number * Game.cemetery_capacity
+            if (hospital_avaiability > 0):
+                Game.score -= hospital_avaiability // 100 # (1) Кайф формула
+            if (church_avaiability > 0):
+                Game.score -= church_avaiability // 1000000
+            if (police_avaiability > 0):
+                Game.score -= police_avaiability // 1000000
+            if (firestation_avaiability > 0):
+                Game.score -= firestation_avaiability // 10000000
+            if (cemetery_avaiability > 0):
+                Game.score -= cemetery_avaiability // 10000000
+            else: 
                 buildings_score = 0
                 for i in range(70):
                     for j in range(i % 2, 70, 2):
