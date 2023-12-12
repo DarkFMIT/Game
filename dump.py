b4c6c5a39ffcef751569b683b0a6a864f26ab854
s0r1k <klimenko.m@phystech.edu>
2023-12-12 22:20:43 +0300

    another coeff

--- dump.py
+++ dump.py
@@ -21,7 +21,7 @@ class Dump(Objects_for_build):
         icon = pygame.image.load(f".\\resources\\Buildings\{icon_name}.png")
         self.dopusc = [0]
         self.dopusc_of_plate = 0
-        self.prise = 10000
+        self.prise = 500
         self.score = 500
         self.icon_name = icon_name
         self.capacity = 0
